a5921311dc4754b92e939b0c4a62d455238f2694
qiming97 <your-email@example.com>
2026-01-07 17:44:56 +0800

    1

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Settings(BaseSettings):
     TWITTER_BEARER_TOKEN: str = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs=1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
     
     # TID 算法服务地址
-    TID_SERVICE_URL: str = "http://localhost:35902/gettid"
+    TID_SERVICE_URL: str = "http://df4b651636e7:35902/gettid"
     
     # 2FA 服务地址
     TWO_FA_SERVICE_URL: str = "https://2fa.live/tok/"
